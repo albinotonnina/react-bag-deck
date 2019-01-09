9fd42946555a3f2cc71ed55d954fd0cf29e49f64
Albino Tonnina <albinotonnina@gmail.com>
2019-01-09 08:21:22 +0000

    no message

--- src/components/index.js
+++ src/components/index.js
@@ -18,3 +18,24 @@ export const RelativeDiv = styled.div`
 export function Title(props) {
   return <AbsoluteDiv>{props.children}</AbsoluteDiv>;
 }
+
+export const TitleLeft = styled.h1`
+  margin: 0;
+  text-align: left;
+`;
+export const Title2Left = styled.h2`
+  margin: 0;
+  text-align: left;
+`;
+
+export const FlexContainer = styled.div`
+  display: flex;
+  width: 50vw;
+`;
+export const FlexItem = styled.div`
+  flex: 1 1 auto;
+`;
+export const Scroller = styled.div`
+  overflow-y: scroll;
+  height: 90vh;
+`;
